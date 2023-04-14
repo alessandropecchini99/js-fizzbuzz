fe1f146c0f7d1e86cb1eddbdfcc11596d7f34c08
alessandropecchini99 <pecchini99@gmail.com>
2023-04-14 18:24:08 +0200

    add 100 squares

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-for (i = 0; i <= 100; i++) {
-    console.log(i)
+const divTable = document.querySelector('.table');
 
+for (i = 0; i <= 100; i++) {
+    console.log(i);
+    divTable.innerHTML = divTable.innerHTML + `<div class="square">${i}</div>`
 }
